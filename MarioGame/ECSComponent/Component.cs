283fef40bf6318fa47e72c09a2f07c074b575e3e
Isaias ROjas <isaias@email.com>
2024-05-20 17:07:01 -0400

    feat: Add a the base component

---
+++ MarioGame/ECSComponent/Component.cs
@@ -0,0 +1,12 @@
+using Microsoft.Xna.Framework;
+
+namespace ECSComponent
+{
+    /// <summary>
+    /// The base class for all components.
+    /// </summary>
+    public abstract class Component
+    {
+
+    }
+}
