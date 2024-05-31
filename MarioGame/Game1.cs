862286a89c4802d43e1effc8eb8c764d0b75270a
Fernando Mauricio Mamani Navarro <wmauchess@gmail.com>
2024-05-31 08:52:45 -0400

    hotfix: resize game

--- MarioGame/Game1.cs
+++ MarioGame/Game1.cs
@@ -1,8 +1,5 @@
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
-using SuperMarioBros.Source.Core;
-using SuperMarioBros.Source.Levels;
-using Services;
 using MarioGame;
 
 namespace SuperMarioBros
@@ -12,11 +9,14 @@ namespace SuperMarioBros
         public GraphicsDeviceManager Graphics { get; }
         public SpriteBatch Batch { get; private set; }
         private WorldGame _world;
-        private WorldInitializer _worldInitializer;
 
         public Game1()
         {
-            Graphics = new GraphicsDeviceManager(this);
+            Graphics = new GraphicsDeviceManager(this)
+            {
+                PreferredBackBufferWidth = 1080,
+                PreferredBackBufferHeight = 720
+            };
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
         }
@@ -25,8 +25,9 @@ namespace SuperMarioBros
         {
             base.Initialize();
             _world = new WorldGame();
-            _worldInitializer = new WorldInitializer(GraphicsDevice);
-            _worldInitializer.Initialize();
+            //TODO: Review
+            //_worldInitializer = new WorldInitializer(GraphicsDevice);
+            //_worldInitializer.Initialize();
         }
 
         protected override void LoadContent()
@@ -36,18 +37,14 @@ namespace SuperMarioBros
 
         protected override void Update(GameTime gameTime)
         {
-           // _world.(gameTime);
             base.Update(gameTime);
-        }// Add the missing import statement
+        }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-         // Declare and initialize _spriteBatch variable
             Batch.Begin();
-            Vector2 scale = new Vector2(1.5f);
-            Batch.Draw(Sprites.Goomba1, new Vector2(100, 100), null, Color.White, 0f, Vector2.Zero, scale, SpriteEffects.None, 0f);
-
+            _world.Draw();
             Batch.End();
             base.Draw(gameTime);
         }
