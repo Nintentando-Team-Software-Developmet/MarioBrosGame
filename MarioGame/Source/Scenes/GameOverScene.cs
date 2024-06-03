0d211702755b62dd2e95fcede4d47fe7b91609d2
Hugo Oropeza <oropezahugo35@gmail.com>
2024-06-03 11:14:36 -0400

    feat: add game over scene

---
+++ MarioGame/Source/Scenes/GameOverScene.cs
@@ -0,0 +1,130 @@
+using System;
+
+using MarioGame;
+using MarioGame.Source.Scenes;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Media;
+
+namespace SuperMarioBros.Source.Scenes;
+
+public class GameOverScene : IScene, IDisposable
+{
+    private bool disposed;
+    private string Screen { get; set; } = "Screen";
+    private int RemainingLives;
+    private int Coins;
+    private string World;
+    private int Time;
+
+    public GameOverScene(int remainingLives, int coins, string world, int time)
+    {
+        RemainingLives = remainingLives;
+        Coins = coins;
+        World = world;
+        Time = time;
+    }
+
+
+    /*
+     * Loads resources needed for the game over scene.
+     * This includes loading game sprites and background music.
+     *
+     * Parameters:
+     *   spriteData: SpriteData object containing content manager for loading resources.
+     *               If null, no resources will be loaded.
+     */
+    public void Load(SpriteData spriteData)
+    {
+        Sprites.Load(spriteData?.content);
+    }
+
+    /*
+     * Unloads resources and performs cleanup operations for the game over scene.
+     * This method is called when the scene is being unloaded or switched.
+     * It prints the current screen information to the console.
+     */
+    public void Unload()
+    {
+        Console.WriteLine(Screen);
+    }
+
+    /*
+     * Draws the gameover scene on the screen.
+     * This method clears the graphics device, then draws various elements
+     * such as information of world, time, etc
+     */
+    public void Draw(SpriteData spriteData)
+    {
+        spriteData?.graphics.GraphicsDevice.Clear(Color.Black);
+
+        spriteData.spriteBatch.Begin();
+        DrawTextWithNumber("COINS", $"{Coins}", 70, 10, spriteData);
+        DrawTextWithNumber("WORLD", World, 550, 10, spriteData);
+        DrawTextWithNumber("TIME", $"{Time}", 900, 10, spriteData);
+        DrawText("GAME OVER", 500, 300, spriteData);
+        DrawText($"MARIO x{RemainingLives}", 500, 330, spriteData);
+
+
+        spriteData.spriteBatch.End();
+    }
+    /*
+     * Draws text followed by a number at the specified position.
+     *
+     * Parameters:
+     *   text: The text to display.
+     *   number: The number to display.
+     *   x: The X-coordinate of the text position.
+     *   y: The Y-coordinate of the text position.
+     *   spriteData: SpriteData object containing graphics device, sprite batch, and font for drawing.
+     *               If null, no drawing will occur.
+     */
+    private static void DrawTextWithNumber(string text, string number, float x, float y, SpriteData spriteData)
+    {
+        Vector2 textPosition = new Vector2(x, y);
+        Vector2 numberPosition = new Vector2(x, y + spriteData.spriteFont.LineSpacing);
+
+        spriteData.spriteBatch.DrawString(spriteData.spriteFont, text, textPosition, Color.White);
+        spriteData.spriteBatch.DrawString(spriteData.spriteFont, number, numberPosition, Color.White);
+    }
+
+    /*
+     * Draws text  at the specified position.
+     *
+     * Parameters:
+     *   text: The text to display.
+     *   x: The X-coordinate of the text position.
+     *   y: The Y-coordinate of the text position.
+     *   spriteData: SpriteData object containing graphics device, sprite batch, and font for drawing.
+     *               If null, no drawing will occur.
+     */
+    private static void DrawText(string text, float x, float y, SpriteData spriteData)
+    {
+        Vector2 textPosition = new Vector2(x, y);
+
+        spriteData.spriteBatch.DrawString(spriteData.spriteFont, text, textPosition, Color.White);
+    }
+
+
+    /*
+     * Performs cleanup operations and releases resources.
+     * This method is called to dispose of the GameOver object.
+     */
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    /*
+    * Releases managed resources if disposing is true.
+    */
+    protected virtual void Dispose(bool disposing)
+    {
+        if (disposed)
+            return;
+
+        disposed = true;
+    }
+}
+
