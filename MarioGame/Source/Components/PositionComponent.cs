0f90f5f34a7a57e0eb53e312d03263dae905b116
GastonGutierrezC <gaston.gutierrez.9964359@usalesiana.edu.bo>
2024-06-10 23:56:49 -0400

    Jump Mario

--- MarioGame/Source/Components/PositionComponent.cs
+++ MarioGame/Source/Components/PositionComponent.cs
@@ -6,13 +6,13 @@ public class PositionComponent : BaseComponent
 {
     public Vector2 Position { get; set; }
     public Vector2 LastPosition { get; set; }
-    public Vector2 LastPosition2 { get; set; }
-    public bool pass { get; set; }
 
+    public bool pass  { get; set; }
+    public bool passR  { get; set; }
+    public bool passBed  { get; set; }
     public PositionComponent(Vector2 initialPosition)
     {
         Position = initialPosition;
-        LastPosition2 = LastPosition2 with { X = initialPosition.X };
-        LastPosition2 = LastPosition2 with { Y = initialPosition.Y };
+
     }
 }
--- MarioGame/Source/Entities/EntityFactory.cs
+++ MarioGame/Source/Entities/EntityFactory.cs
@@ -39,7 +39,7 @@ namespace SuperMarioBros.Source.Entities
                 case EntityType.PLAYER:
                     entity.AddComponent(new PlayerComponent());
                     entity.AddComponent(new VelocityComponent(Vector2.Zero));
-                    entity.AddComponent(new GravityComponent(9.8f));
+                 //   entity.AddComponent(new GravityComponent(9.8f));
                     entity.AddComponent(new AnimationComponent(Animations.entityTextures[entityData.name]));
                     entity.AddComponent(new InputComponent());
                     entity.AddComponent(new CameraComponent(
