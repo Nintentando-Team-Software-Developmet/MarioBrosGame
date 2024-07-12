038c4eebe656525dbc34f66b91ffad165dd12226
JuanPabloArequipa <juan.arequipa.12833847@usalesiana.edu.bo>
2024-07-12 11:02:23 -0400

    fix: update highscore at winning

--- MarioGame/Source/Scenes/WinScene.cs
+++ MarioGame/Source/Scenes/WinScene.cs
@@ -60,6 +60,7 @@ public class WinScene : IScene, IDisposable
 
         if (_displayTime >= MaxDisplayTime)
         {
+            _progressDataManager.UpdateHighScore();
             sceneManager.ChangeScene(SceneName.MainMenu);
         }
     }
